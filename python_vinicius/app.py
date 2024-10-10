04055ac84dc5cac515c581cc4d7d3159cee00b2a
VVs-Lang <viniciusveloso007@gmail.com>
2024-10-10 15:08:54 -0300

    Add files via upload

---
+++ python_vinicius/app.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Digite seu primeiro numero"))
+numero2 = int(input("Digite seu segundo numero"))
+soma = numero1 + numero2
+subtração = numero1 - numero2
+divisao = numero1 // numero2
+multiplicação = numero1 * numero2
+print("soma: ", soma)
+print("subtração: ", subtração)
+print("divisão: ", divisao)
+print("multiplicação: ", multiplicação)
